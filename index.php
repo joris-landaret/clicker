633dc79e39792886b26237288b1bdd3050705109
joris-landaret <joris.landaret@laplateforme.io>
2023-02-27 14:38:35 +0100

    ajout de classe et d'id aux balise

--- index.php
+++ index.php
@@ -6,24 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clicker</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 
     <main>
 
-        <div>
-            <div>
-                <img src="image/ps2.jpg" alt="">
+        <div id="">
+            <div id="enemis">
+                <img id="clicker" class="img" src="image/ps2.jpg" alt="">
             </div>
 
-            <div>
-                <div><img src="image/médecin.png" alt=""></div>
-                <div><img src="image/infiltré.png" alt=""></div>
-                <div><img src="image/assaut_lourd.png" alt=""></div>
-                <div><img src="image/poing.png" alt=""></div>
-                <div><img src="image/assaut_léger.png" alt=""></div>
-                <div><img src="image/ingénieur.png" alt=""></div>
+            <div id="alliés">
+                <div><img class="img" src="image/médecin.png" alt=""></div>
+                <div><img class="img" src="image/infiltré.png" alt=""></div>
+                <div><img class="img" src="image/assaut_lourd.png" alt=""></div>
+                <div><img id="principal" class="img" src="image/poing.png" alt=""></div>
+                <div><img class="img" src="image/assaut_léger.png" alt=""></div>
+                <div><img class="img" src="image/ingénieur.png" alt=""></div>
             </div>
         </div>
 
