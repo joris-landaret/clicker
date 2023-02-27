727a00325f33b1f236a991c8d8d1d9d2d3bdbc52
joris-landaret <joris.landaret@laplateforme.io>
2023-02-27 16:46:41 +0100

    index.php : ajou d'image d'id et classe

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 <img id="clicker" src="image/ps2.jpg" alt="">
             </div>
 
-            <div id="alliés">
+            <div id="allies">
                 <div class="carte"><img class="img" src="image/médecin.png" alt=""></div>
                 <div class="carte"><img class="img" src="image/infiltré.png" alt=""></div>
                 <div class="carte"><img class="img" src="image/assaut_lourd.png" alt=""></div>
@@ -31,25 +31,25 @@
 
         <div class="shop">
             <h1>Arme</h1>
-            <div class="armes">
-                <div class="carte_arme"><img class="img_arme" src="image/Cart-5-S_2.png" alt=""></div>
-                <div class="carte_arme"><img class="img_arme" src="image/Arbaléte-MG-A1_2.png" alt=""></div>
-                <div class="carte_arme"><img class="img_arme" src="image/MG-HBR1-Dragon_2.png" alt=""></div>
-                <div class="carte_arme"><img class="img_arme" src="image/M-H1-Vigie_2.png" alt=""></div>
-                <div class="carte_arme"><img class="img_arme" src="image/Percuteur-T2_2.png" alt=""></div>
-                <div class="carte_arme"><img class="img_arme" src="image/Pitié-MRC3_2.png" alt=""></div>
+            <div id="shop_armes">
+                <div class="carte_arme"><img class="img_armes" src="image/Cart-5-S_2.png" alt=""></div>
+                <div class="carte_arme"><img class="img_armes" src="image/Arbaléte-MG-A1_2.png" alt=""></div>
+                <div class="carte_arme"><img class="img_armes" src="image/MG-HBR1-Dragon_2.png" alt=""></div>
+                <div class="carte_arme"><img class="img_armes" src="image/M-H1-Vigie_2.png" alt=""></div>
+                <div class="carte_arme"><img class="img_armes" src="image/Percuteur-T2_2.png" alt=""></div>
+                <div class="carte_arme"><img class="img_armes" src="image/Pitié-MRC3_2.png" alt=""></div>
             </div>
         </div>
 
-        <div>
+        <div class="shop">
             <h1>Alliés</h1>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div id="shop_allies">
+                <div class="carte"><img class="img_allies" src="image/médecin.png" alt=""></div>
+                <div class="carte"><img class="img_allies" src="image/ingénieur.png" alt=""></div>
+                <div class="carte"><img class="img_allies" src="image/infiltré.png" alt=""></div>
+                <div class="carte"><img class="img_allies" src="image/assaut_léger.png" alt=""></div>
+                <div class="carte"><img class="img_allies" src="image/assaut_lourd.png" alt=""></div>
+                <div class="carte"><img class="img_allies" src="image/xam.png" alt=""></div>
             </div>
         </div>
 
