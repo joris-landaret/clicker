91657296b1f47354f9aba88182703194c94cea5e
joris-landaret <joris.landaret@laplateforme.io>
2023-02-28 11:30:20 +0100

    ajout d'un fichier clicker.js et le lié au fichier index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clicker</title>
     <link rel="stylesheet" href="css/style.css">
+    <script async src="clicker.js"></script>
 </head>
 
 <body>
